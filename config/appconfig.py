c2b104c020c08f77756c7593df42496e3187cb6b
pendragon <16505118+austinHeisleyCook@users.noreply.github.com>
2021-02-03 18:28:45 -0800

    Add files via upload
    proejct stats aded css wip

--- config/appconfig.py
+++ config/appconfig.py
@@ -1,19 +1,16 @@
 from datetime import datetime, time
 from typing import Dict
-from flask import(Flask, render_template, request, Response, jsonify, redirect)
+from flask import (Flask, render_template, request, Response, jsonify, redirect)
 import sqlite3
-isAdmin = False
+import secrets
+import datetime
+
+
+
 app = Flask(__name__)
+app.secret_key = secrets.token_urlsafe()
 db = sqlite3.connect("/home/aheisleycook/journalApp/db/journal.db")
 
-
-@app.route("/login")
-def Login():
-    user = request.form['username']
-    password = request.form['password']
-    db = sqlite3.connect("/home/aheisleycook/journalApp/db/journal.db")
-    conn = db.cursor()
-    f = conn.fetchone()
 
 @app.route("/delete", methods=["post"])
 def delete():
@@ -21,9 +18,10 @@ def delete():
     db = sqlite3.connect("/home/aheisleycook/journalApp/db/journal.db")
     sql = 'DELETE FROM entry WHERE title=?'
     conn = db.cursor()
-    conn.execute(sql,(stitle,))
+    conn.execute(sql, (stitle,))
     db.commit()
     return redirect("/")
+
 
 @app.route("/search", methods=["get"])
 def Search():
@@ -31,34 +29,40 @@ def Search():
     db = sqlite3.connect("/home/aheisleycook/journalApp/db/journal.db")
     sql = 'Select * from entry where title=?'
     conn = db.cursor()
-    conn.execute(sql(query,))
-    s = conn.fetchall()
-    return render_template("index.html",s=s)
+    conn.execute(sql(query, ))
+    a = conn.fetchall()
+    return render_template("index.html", a=a)
 
-    
-    
 
-@app.route("/",methods=["get"])
+@app.route("/", methods=["get"])
 def showEntry():
     db = sqlite3.connect("/home/aheisleycook/journalApp/db/journal.db")
     conn = db.cursor()
     conn.execute("select * from entry")
-    a =  conn.fetchall()
-    return  render_template("index.html",a=a)
+    a = conn.fetchall()
+    return render_template("index.html", a=a)
+
+
 @app.route("/add", methods=["post"])
 def addEntry():
     title = request.form["title"]
     desc = request.form["desc"]
     db = sqlite3.connect("/home/aheisleycook/journalApp/db/journal.db")
     conn = db.cursor()
-    conn.execute("insert into entry values(?,?)",(title,desc))
+    conn.execute("insert into entry values(?,?)", (title, desc))
     a = conn.fetchall()
-    return  render_template("index.html",a=a)
+    return render_template("index.html", a=a)
 
-@app.route("/log")
-def EntryToJson():
-    return datetime.now
 
-    
-    
+@app.route("/sendtolog")
+def logrecord():
+    db = sqlite3.connect("/home/aheisleycook/journalApp/db/journal.db")
+    conn = db.cursor()
+    conn.execute("select * from entry")
+    a = conn.fetchall()
+    for Record in a:
+        with open("entries.txt", "a") as t:
+            t.write("title" + Record[0] + "\ntitel:" + Record[1])
+
+
 app.run(host="0.0.0.0", port=8080)
