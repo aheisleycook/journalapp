28dbb9264f96e1fb637aba3325b6a18f0b771e3f
pendragon <16505118+austinHeisleyCook@users.noreply.github.com>
2021-02-14 17:14:50 -0800

    migrated main db to from sqlit to mysql

--- appconfig.py
+++ appconfig.py
@@ -7,32 +7,42 @@ import pymysql
 from flask import (Flask, render_template, request, Response, jsonify, redirect)
 import sqlite3
 import secrets
-import datetime
-
-
+import pymysql
 
 app = Flask(__name__)
 app.secret_key = secrets.token_urlsafe()
-db = sqlite3.connect("/home/aheisleycook/journalApp/db/journal.db")
-
+db = pymysql.Connection(user="aheisleycook",passwd="A7147084155o!")
+@app.route("/login")
+def login():
+    username = request.form['username']
+    password  = request.form['password']
+    conn = db.cursor()
+    sql = 'Select * from entry where title=? where password=?'
+    conn.execute(sql(username, password))
+    if conn:
+        session["logged_in"] = True
+    else:
+        session["logged_in"] = False
 
 @app.route("/delete", methods=["post"])
 def delete():
     stitle = request.form["title"]
-    db = sqlite3.connect("/home/aheisleycook/journalApp/db/journal.db")
     sql = 'DELETE FROM entry WHERE title=?'
     conn = db.cursor()
     conn.execute(sql, (stitle,))
     db.commit()
     return redirect("/")
+"""
+this is my current project routing 
 
+"""
 
 @app.route("/search", methods=["get"])
 def Search():
     query = request.args["entry"]
-    db = sqlite3.connect("/home/aheisleycook/journalApp/db/journal.db")
     sql = 'Select * from entry where title=?'
     conn = db.cursor()
+
     conn.execute(sql(query, ))
     a = conn.fetchall()
     return render_template("index.html", a=a)
@@ -40,7 +50,6 @@ def Search():
 
 @app.route("/", methods=["get"])
 def showEntry():
-    db = sqlite3.connect("/home/aheisleycook/journalApp/db/journal.db")
     conn = db.cursor()
     conn.execute("select * from entry")
     a = conn.fetchall()
@@ -51,7 +60,6 @@ def showEntry():
 def addEntry():
     title = request.form["title"]
     desc = request.form["desc"]
-    db = sqlite3.connect("/home/aheisleycook/journalApp/db/journal.db")
     conn = db.cursor()
     conn.execute("insert into entry values(?,?)", (title, desc))
     a = conn.fetchall()
@@ -60,7 +68,6 @@ def addEntry():
 
 @app.route("/sendtolog")
 def logrecord():
-    db = sqlite3.connect("/home/aheisleycook/journalApp/db/journal.db")
     conn = db.cursor()
     conn.execute("select * from entry")
     a = conn.fetchall()
@@ -69,4 +76,4 @@ def logrecord():
             t.write("title" + Record[0] + "\ntitel:" + Record[1])
 
 
-app.run(host="0.0.0.0", port=8080)
+app.run(host="0.0.0.0", port=8086)
